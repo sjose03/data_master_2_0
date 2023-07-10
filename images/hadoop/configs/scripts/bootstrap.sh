0ad980bcba3935d0a26d85890ec5e17e2a220138
Jose paulo <jose.petrolio0308@gmail.com>
2023-07-10 00:55:12 -0300

    hadoop image initial setup

---
+++ images/hadoop/configs/scripts/bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Este trecho rodará independente de termos um container master ou
+# worker. Necesário para funcionamento do HDFS e para comunicação
+# dos containers/nodes.
+/etc/init.d/ssh start
+
+export HDFS_NAMENODE_USER=root
+export HDFS_DATANODE_USER=root
+export HDFS_SECONDARYNAMENODE_USER=root
+export YARN_RESOURCEMANAGER_USER=root
+export YARN_NODEMANAGER_USER=root
+
+# Formatamos o namenode
+hdfs namenode -format
+
+# Iniciamos os serviços
+$HADOOP_HOME/sbin/start-dfs.sh
+$HADOOP_HOME/sbin/start-yarn.sh
+
+hdfs dfs -mkdir /datasets
+hdfs dfs -mkdir /datasets_processed
+
+
+while :; do sleep 2073600; done
