292ad59796df9e1b1ee98b4a52639c3c51952ee9
Jose paulo <jose.petrolio0308@gmail.com>
2023-07-15 23:15:35 -0300

    config hadoop hue

--- images/hadoop/configs/scripts/bootstrap.sh
+++ images/hadoop/configs/scripts/bootstrap.sh
@@ -10,6 +10,8 @@ export HDFS_DATANODE_USER=root
 export HDFS_SECONDARYNAMENODE_USER=root
 export YARN_RESOURCEMANAGER_USER=root
 export YARN_NODEMANAGER_USER=root
+useradd -g sudo jose
+useradd -g sudo hue
 
 # Formatamos o namenode
 hdfs namenode -format
@@ -18,8 +20,17 @@ hdfs namenode -format
 $HADOOP_HOME/sbin/start-dfs.sh
 $HADOOP_HOME/sbin/start-yarn.sh
 
+
+
+
 hdfs dfs -mkdir /datasets
 hdfs dfs -mkdir /datasets_processed
+hdfs dfs -mkdir /user
+hdfs dfs -mkdir /user/hue
+hdfs dfs -mkdir /user/jose
+# hdfs dfs -chown -R jose:root /user/jose
+# hdfs dfs -chown -R hue:root /user/hue
+hdfs dfsadmin -refreshUserToGroupsMappings
 
 
 while :; do sleep 2073600; done
